fc3433fda02cb553a16728f672e58def59f2e093
Theis194 <53541469+Theis194@users.noreply.github.com>
2023-03-27 17:34:32 +0200

    Mongodb exampels created

--- MongoDBTesting/mongoTest.js
+++ MongoDBTesting/mongoTest.js
@@ -37,6 +37,22 @@ async function main(){
  
         // Make the appropriate DB calls
         await  listDatabases(client);
+        await getEntry(client, "Black Panther");
+        /* await createEntry(client, 
+            {
+                name: "What ever!"
+            });
+        await createMultipleEntrys(client, [
+            {
+                name: "Captain America: civil war"
+            },
+            {
+                name : "Black Panther"
+            },
+            {
+                name : "Black Panther: Wakanda for Ever"
+            }
+        ]); */
  
     } catch (e) {
         console.error(e);
@@ -47,9 +63,37 @@ async function main(){
 
 main().catch(console.error);
 
+// This function takes a MongoClient and prints out a list of all the databases on that client.
 async function listDatabases(client){
     databasesList = await client.db().admin().listDatabases();
  
     console.log("Databases:");
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+}
+
+// This function takes a MongoClient and a newEntry and inserts the entry in to a pre specified collection
+async function createEntry(client, newEntry) {
+    const result = await client.db("theis").collection("movie").insertOne(newEntry);
+    console.log(`New entry created with the following id: ${result.insertedId}`);
+}
+
+// This function takes a MongoClient and an array of newEntrys and inserts the entrys in to a pre specified collection
+async function createMultipleEntrys(client, newEntrys) {
+    const result = await client.db("theis").collection("movie").insertMany(newEntrys);
+
+    console.log(`${result.insertedCount} new listing(s) created with the following id(s):`);
+    console.log(result.insertedIds); 
+}
+
+// This function takes a Mongoclient and a nameOfEntry and prints out the entry found 
+// this can be expanded by returning the entry found
+async function getEntry(client, nameOfEntry) {
+    const result = await client.db("theis").collection("movie").findOne({name: nameOfEntry});
+
+    if (result) {
+        console.log(`Found a entry in the collection with the name '${nameOfEntry}':`);
+        console.log(result);
+    } else {
+        console.log(`No entrys found with the name '${nameOfEntry}'`);
+    }
+}
